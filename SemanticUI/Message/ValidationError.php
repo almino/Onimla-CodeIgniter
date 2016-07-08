ae719e17593b1d2ce701145c30dd2a694b434ef1
Almino Melo <almino.melo@gmail.com>
2016-07-08 16:10:11 -0300

    Correção de erro (atualização da classe Element).

--- SemanticUI/Message/ValidationError.php
+++ SemanticUI/Message/ValidationError.php
@@ -16,7 +16,7 @@ class ValidationError extends Message {
             # Get ID
             $id = $field->id();
             # Get name
-            $field = $field->name()->getValue();
+            $field = $field->name();
         }
 
         # Get error message
