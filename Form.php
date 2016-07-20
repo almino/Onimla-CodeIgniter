fc1e0cd671569b4d2b28371541494efcde9f468c
Almino Melo <almino.melo@gmail.com>
2016-07-20 16:16:50 -0300

    Desacoplar.

--- Form.php
+++ Form.php
@@ -6,7 +6,7 @@ class Form extends \Onimla\HTML\Form {
 
     public function __construct($method = FALSE, $action = FALSE) {
         parent::__construct($method, $action);
-        $this->prepend(new \Onimla\HTML\Hidden(get_instance()->security->get_csrf_token_name(), get_instance()->security->get_csrf_hash()));
+        $this->prepend(new CrossSiteRequestForgery());
     }
 
 }
