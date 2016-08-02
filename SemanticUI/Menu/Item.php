02eb6a9c8956fde604f31edb0c67fce5d3929842
Almino Melo <almino.melo@gmail.com>
2016-08-02 19:19:21 -0300

    Toda vez que o atributo `href` é alterado, verifica se o item deve ficar ativo.

--- SemanticUI/Menu/Item.php
+++ SemanticUI/Menu/Item.php
@@ -11,18 +11,28 @@ class Item extends BaseItem {
 
     public function __construct($text = FALSE, $href = FALSE, $title = FALSE) {
         parent::__construct($text, $href, $title);
+    }
 
-        if (function_exists('current_url') AND current_url() == $href) {
+    /**
+     * <strong>WARNING!</strong> Uses CodeIgniter's <code>site_url</code> function
+     * @param type $url
+     */
+    public function href($url = FALSE) {
+        $url = func_get_args();
+
+        if (function_exists('current_url') AND current_url() == $url) {
             $this->setActive();
         } elseif ($this->ignoreSegment > 0 AND function_exists('site_url') AND function_exists('current_url')) {
             $segments = new stdClass();
             $segments->current = array_slice(explode('/', trim(substr(current_url(), strlen(site_url())), '/')), 0, $this->ignoreSegment - 1);
-            $segments->href = array_slice(explode('/', trim(substr($href, strlen(site_url())), '/')), 0, $this->ignoreSegment - 1);
+            $segments->href = array_slice(explode('/', trim(substr($url, strlen(site_url())), '/')), 0, $this->ignoreSegment - 1);
 
             if (implode('/', $segments->current) == implode('/', $segments->href)) {
                 $this->setActive();
             }
         }
+
+        parent::href(site_url($url));
     }
 
 }
