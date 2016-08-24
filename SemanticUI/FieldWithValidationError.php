ae51b0139269b2cc2eda2609efb1d8a428c7f611
Almino Melo <almino.melo@gmail.com>
2016-08-24 14:45:16 -0300

    Adota a interface `UserInput`.

--- SemanticUI/FieldWithValidationError.php
+++ SemanticUI/FieldWithValidationError.php
@@ -3,18 +3,18 @@
 namespace Onimla\CodeIgniter\SemanticUI;
 
 use Onimla\HTML\Node;
-use Onimla\SemanticUI\Form\Field;
+use Onimla\HTML\Polymorphism\UserInput;
 use Onimla\CodeIgniter\SemanticUI\Message\ValidationError;
 
 class FieldWithValidationError extends Node {
 
-    public function __construct(Field $field, $error = NULL) {
+    public function __construct(UserInput $field, $error = NULL) {
         parent::__construct();
         $this->field = $field;
 
         if ($error !== NULL) {
             $this->message = $error;
-        } elseif (form_error($field->attr('name'))) {
+        } elseif (form_error($field->name())) {
             $this->message = new ValidationError($field);
         }
     }
