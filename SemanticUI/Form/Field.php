6464fc10050d17174dc8f91c8292fa67eaaaafb6
Almino Melo <almino.melo@gmail.com>
2016-10-04 18:51:12 -0300

    Evita duplicar a classe com o erro.

--- SemanticUI/Form/Field.php
+++ SemanticUI/Form/Field.php
@@ -7,7 +7,7 @@ use Onimla\SemanticUI\Form\Field as BaseField;
 class Field extends BaseField {
 
     public function __toString() {
-        if (form_error($this->name())) {
+        if (!$this->container->hasClass('error') AND form_error($this->name())) {
             $this->error();
         }
 
